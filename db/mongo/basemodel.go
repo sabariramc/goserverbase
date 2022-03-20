46d293e80e2f6d0bdb40d67713f43161cc8694d8
Sabariram <sabariram.c@gmail.com>
2022-03-20 09:57:06 +0530

    Added base mongo document

--- db/mongo/basemodel.go
+++ db/mongo/basemodel.go
@@ -6,10 +6,14 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type BaseMongoDocument struct {
+	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
+	CreatedBy string    `json:"createdBy" bson:"createdBy"`
+	UpdatedBy string    `json:"updatedBy" bson:"updatedBy"`
+}
+
 type BaseMongoModel struct {
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
-	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
-	CreatedBy string             `json:"createdBy" bson:"createdBy"`
-	UpdatedBy string             `json:"updatedBy" bson:"updatedBy"`
+	ID primitive.ObjectID `json:"_id" bson:"_id"`
+	BaseMongoDocument
 }
