92a5a41f2bc046a827b6b886cbe8e4f5fb8df51c
Sabariram Chinnathambi <c.sabariram@gmail.com>
2023-05-03 15:48:30 +0530

    Added http utils

--- utils/httputil/httputils.go
+++ utils/httputil/httputils.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"reflect"
 	"time"
@@ -22,26 +21,28 @@ type LogConfig struct {
 }
 
 var DefaultLogConfig = &LogConfig{
-	MaxContentLength: 1024,
+	MaxContentLength: 10240,
 }
 
 type HttpClient struct {
-	*retryablehttp.Client
-	log       *log.Logger
-	LogConfig LogConfig
+	*http.Client
+	log          *log.Logger
+	LogConfig    LogConfig
+	RetryMax     int
+	RetryWaitMin time.Duration
+	RetryWaitMax time.Duration
 }
 
 func NewDefaultHttpClient(log *log.Logger) *HttpClient {
-	return NewHttpClient(time.Minute, log, *DefaultLogConfig)
+	return NewHttpClient(time.Minute, log, *DefaultLogConfig, 4, time.Second*1, time.Second*5)
 }
 
-func NewHttpClient(timeout time.Duration, log *log.Logger, logConfig LogConfig) *HttpClient {
+func NewHttpClient(timeout time.Duration, log *log.Logger, logConfig LogConfig, retryMax int, retryWaitMin, retryWaitMax time.Duration) *HttpClient {
 	t := http.DefaultTransport.(*http.Transport).Clone()
 	t.MaxIdleConns = 100
 	t.MaxConnsPerHost = 100
 	t.MaxIdleConnsPerHost = 100
-	c := &HttpClient{Client: retryablehttp.NewClient(), log: log, LogConfig: logConfig}
-	c.HTTPClient.Transport = t
+	c := &HttpClient{Client: &http.Client{Transport: t}, log: log, LogConfig: logConfig, RetryMax: retryMax, RetryWaitMin: retryWaitMin, RetryWaitMax: retryWaitMax}
 	return c
 }
 
@@ -55,40 +56,37 @@ func (h *HttpClient) Validator(resBody interface{}) error {
 	return nil
 }
 
-func (h *HttpClient) Encode(ctx context.Context, data interface{}) (interface{}, io.ReadCloser, error) {
+func (h *HttpClient) Encode(ctx context.Context, data interface{}) (interface{}, io.Reader, error) {
 	var print interface{}
-	var body io.ReadCloser
+	var body io.Reader
 	if data != nil {
 		switch v := data.(type) {
 		case string:
 			if int64(len(v)) <= h.LogConfig.MaxContentLength {
 				print = v
 			}
-			body = io.NopCloser(bytes.NewReader([]byte(v)))
+			body = bytes.NewReader([]byte(v))
 		case []byte:
 			if int64(len(v)) <= h.LogConfig.MaxContentLength {
 				print = string(v)
 			}
-			body = io.NopCloser(bytes.NewReader(v))
+			body = bytes.NewReader(v)
 		case io.ReadCloser:
 			body = v
 		case io.Reader:
-			body = io.NopCloser(v)
+			body = v
 		default:
 			rv := reflect.ValueOf(v)
-			if rv.Kind() != reflect.Struct {
-				return nil, nil, fmt.Errorf("payload is not a struct/string/[]byte/io.ReadCloser/io.Reader object")
-			}
 			if int64(rv.Type().Size()) <= h.LogConfig.MaxContentLength {
 				print = data
 			}
 			buff := &bytes.Buffer{}
 			err := json.NewEncoder(buff).Encode(&data)
-			body = io.NopCloser(buff)
 			if err != nil {
 				h.log.Error(ctx, "Http.Do.PayloadEncoding", err)
 				return nil, nil, fmt.Errorf("http.do.PayloadEncoding: %w", err)
 			}
+			body = buff
 		}
 	}
 	return print, body, nil
@@ -115,25 +113,46 @@ func (h *HttpClient) Decode(ctx context.Context, body []byte, data interface{}) 
 	return newBuff, nil
 }
 
-func (h *HttpClient) Do(ctx context.Context, method, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+func (h *HttpClient) Get(ctx context.Context, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+	return h.Call(ctx, http.MethodGet, url, reqBody, resBody, headers)
+}
+
+func (h *HttpClient) Post(ctx context.Context, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+	return h.Call(ctx, http.MethodPost, url, reqBody, resBody, headers)
+}
+
+func (h *HttpClient) Put(ctx context.Context, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+	return h.Call(ctx, http.MethodPut, url, reqBody, resBody, headers)
+}
+
+func (h *HttpClient) Patch(ctx context.Context, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+	return h.Call(ctx, http.MethodPatch, url, reqBody, resBody, headers)
+}
+
+func (h *HttpClient) Delete(ctx context.Context, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
+	return h.Call(ctx, http.MethodDelete, url, reqBody, resBody, headers)
+}
+
+func (h *HttpClient) Call(ctx context.Context, method, url string, reqBody, resBody interface{}, headers map[string]string) (*http.Response, error) {
 	err := h.Validator(resBody)
 	if err != nil {
 		return nil, err
 	}
+	var req *http.Request
 	reqPrint, body, err := h.Encode(ctx, reqBody)
 	if err != nil {
 		return nil, err
 	}
-	req, err := retryablehttp.NewRequestWithContext(ctx, method, url, nil)
+	req, err = http.NewRequestWithContext(ctx, method, url, body)
 	if err != nil {
 		h.log.Error(ctx, "Http.Do.RequestCreation", err)
 		return nil, fmt.Errorf("http.do.requestCreation: %w", err)
 	}
-	log.SetCorrelationHeader(ctx, req.Request)
+	log.SetCorrelationHeader(ctx, req)
 	for key, val := range headers {
 		req.Header.Add(key, val)
 	}
-	if reqPrint == nil {
+	if reqBody != nil && reqPrint == nil {
 		h.log.Debug(ctx, "Request payload is not printed : either it is a interface or violates MaxContentLength", err)
 	}
 	h.log.Debug(ctx, "Request", map[string]interface{}{
@@ -142,29 +161,97 @@ func (h *HttpClient) Do(ctx context.Context, method, url string, reqBody, resBod
 		"headers": req.Header,
 		"body":    reqPrint,
 	})
-	if body != nil {
-		req.Body = body
-	}
-	r, err := h.Client.Do(req)
+	r, err := h.Do(req)
 	if err != nil {
 		h.log.Error(ctx, "http.do.networkCall", err)
-		return nil, fmt.Errorf("http.do.networkCall: %w", err)
+		return r, fmt.Errorf("http.do.networkCall: %w", err)
+	} else if r == nil {
+		return r, err
 	}
 	logRes := map[string]interface{}{
 		"statusCode": r.StatusCode,
+		"headers":    r.Header,
 	}
-	defer body.Close()
-	blobBody, _ := ioutil.ReadAll(body)
-	if r.StatusCode == http.StatusNoContent || r.ContentLength <= 0 {
+	defer r.Body.Close()
+	if r.StatusCode == http.StatusNoContent || r.ContentLength <= 0 || r.StatusCode > 299 {
 		h.log.Debug(ctx, "Response", logRes)
 		return r, nil
 	}
+	resBlob, _ := io.ReadAll(r.Body)
+	r.Body, err = h.Decode(ctx, resBlob, resBody)
 	if r.ContentLength > h.LogConfig.MaxContentLength {
 		h.log.Debug(ctx, "Response payload is not printed - violets MaxContentLength config", nil)
 	} else {
-		logRes["body"] = string(blobBody)
+		if err != nil {
+			logRes["body"] = string(resBlob)
+		} else {
+			logRes["body"] = resBody
+		}
 	}
 	h.log.Debug(ctx, "Response", logRes)
-	r.Body, err = h.Decode(ctx, blobBody, resBody)
 	return r, err
+}
+
+func (h *HttpClient) Do(req *http.Request) (*http.Response, error) {
+
+	var resp *http.Response
+	var attempt int
+	var shouldRetry bool
+	var doErr, respErr, checkErr error
+	var reqBody, resBlob []byte
+	if req.ContentLength > 0 {
+		reqBody, _ = io.ReadAll(req.Body)
+	}
+	for i := 0; ; i++ {
+		doErr, respErr = nil, nil
+		attempt++
+		req.Body = io.NopCloser(bytes.NewReader(reqBody))
+		resp, doErr = h.Client.Do(req)
+		shouldRetry, checkErr = retryablehttp.DefaultRetryPolicy(req.Context(), resp, doErr)
+		if !shouldRetry || checkErr != nil {
+			break
+		}
+		remain := h.RetryMax - i
+		if remain <= 0 {
+			break
+		}
+		wait := retryablehttp.DefaultBackoff(h.RetryWaitMin, h.RetryWaitMax, i, resp)
+		if resp != nil && resp.ContentLength > 0 {
+			defer resp.Body.Close()
+			resBlob, _ = io.ReadAll(resp.Body)
+			h.log.Notice(req.Context(), fmt.Sprintf("request failed with status code %v retry %v of %v in %vms", resp.StatusCode, i+1, h.RetryMax, wait.Milliseconds()), string(resBlob))
+		} else if doErr != nil {
+			h.log.Notice(req.Context(), fmt.Sprintf("request failed with error - retry %v of %v in %vms", i+1, h.RetryMax, wait.Milliseconds()), doErr)
+		} else {
+			h.log.Notice(req.Context(), fmt.Sprintf("request failed - retry %v of %v in %vms", i+1, h.RetryMax, wait.Milliseconds()), nil)
+		}
+		timer := time.NewTimer(wait)
+		select {
+		case <-req.Context().Done():
+			timer.Stop()
+			h.Client.CloseIdleConnections()
+			return nil, req.Context().Err()
+		case <-timer.C:
+		}
+
+	}
+
+	// this is the closest we have to success criteria
+	if doErr == nil && respErr == nil && checkErr == nil && !shouldRetry {
+		return resp, nil
+	}
+
+	defer h.Client.CloseIdleConnections()
+
+	var err error
+	if checkErr != nil {
+		err = checkErr
+	} else if respErr != nil {
+		err = respErr
+	} else {
+		err = doErr
+	}
+
+	return resp, err
+
 }
--- utils/httputil/httputils_test.go
+++ utils/httputil/httputils_test.go
@@ -3,6 +3,7 @@ package httputil_test
 import (
 	"context"
 	"errors"
+	"net/http"
 	"testing"
 
 	"github.com/sabariramc/goserverbase/v2/log"
@@ -34,34 +35,115 @@ func GetCorrelationContext() context.Context {
 	return ctx
 }
 
+const URL = "http://localhost:64000/service/a/b"
+const RetryURL = "http://localhost:3000/service/error/b"
+const ErrURL = "http://localhost:80/service/error/b"
+
 func TestHttpUtilGet(t *testing.T) {
 	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
 	data := make(map[string]any)
-	res, err := client.Do(GetCorrelationContext(), "GET", "https://d6o0fhi2nl.execute-api.ap-south-1.amazonaws.com/dev/echo/go/base", nil, &data, nil)
+	res, err := client.Get(GetCorrelationContext(), URL, nil, &data, nil)
 	assert.NilError(t, err)
 	assert.Equal(t, res.StatusCode, 200)
 }
 
+func TestHttpUtilGetWithBody(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Get(GetCorrelationContext(), URL, &body, &data, map[string]string{"Content-Type": "application/json"})
+	assert.NilError(t, err)
+	assert.Equal(t, res.StatusCode, 200)
+	assert.DeepEqual(t, body, data["body"])
+	assert.DeepEqual(t, http.MethodGet, data["method"])
+}
+
 func TestHttpUtilUnwrapError(t *testing.T) {
 	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
-	data := ""
+	data := 0
 	body := map[string]string{
 		"tag": "Test",
 	}
-	res, err := client.Do(GetCorrelationContext(), "POST", "https://d6o0fhi2nl.execute-api.ap-south-1.amazonaws.com/dev/echo/go/base", &body, &data, nil)
-	if !errors.Is(err, httputil.ErrResponseUnmarshal) {
-		t.Fail()
+	_, err := client.Post(GetCorrelationContext(), URL, &body, &data, map[string]string{"Content-Type": "application/json"})
+	if errors.Is(err, httputil.ErrResponseUnmarshal) {
+		return
 	}
-	assert.Equal(t, res.StatusCode, 200)
+	t.Fail()
 }
 
 func TestHttpUtilPost(t *testing.T) {
 	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
 	data := make(map[string]any)
-	body := map[string]string{
+	body := map[string]any{
 		"tag": "Test",
 	}
-	res, err := client.Do(GetCorrelationContext(), "POST", "https://d6o0fhi2nl.execute-api.ap-south-1.amazonaws.com/dev/echo/go/base", &body, &data, nil)
+	res, err := client.Post(GetCorrelationContext(), URL, &body, &data, nil)
 	assert.NilError(t, err)
 	assert.Equal(t, res.StatusCode, 200)
+	assert.DeepEqual(t, body, data["body"])
+	assert.DeepEqual(t, http.MethodPost, data["method"])
+}
+
+func TestHttpUtilPatch(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Patch(GetCorrelationContext(), URL, &body, &data, nil)
+	assert.NilError(t, err)
+	assert.Equal(t, res.StatusCode, 200)
+	assert.DeepEqual(t, body, data["body"])
+	assert.DeepEqual(t, http.MethodPatch, data["method"])
+}
+
+func TestHttpUtilPut(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Put(GetCorrelationContext(), URL, &body, &data, nil)
+	assert.NilError(t, err)
+	assert.Equal(t, res.StatusCode, 200)
+	assert.DeepEqual(t, body, data["body"])
+	assert.DeepEqual(t, http.MethodPut, data["method"])
+}
+
+func TestHttpUtilDelete(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Delete(GetCorrelationContext(), URL, &body, &data, nil)
+	assert.NilError(t, err)
+	assert.Equal(t, res.StatusCode, 200)
+	assert.DeepEqual(t, body, data["body"])
+	assert.DeepEqual(t, http.MethodDelete, data["method"])
+}
+
+func TestHttpUtilRetry(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Call(GetCorrelationContext(), "POST", RetryURL, &body, &data, nil)
+	assert.NilError(t, err)
+	assert.Equal(t, res.StatusCode, 500)
+}
+
+func TestHttpUtilRetryError(t *testing.T) {
+	client := httputil.NewDefaultHttpClient(HttpUtilTestLogger)
+	data := make(map[string]any)
+	body := map[string]any{
+		"tag": "Test",
+	}
+	res, err := client.Call(GetCorrelationContext(), "POST", ErrURL, &body, &data, nil)
+	if err == nil || res != nil {
+		t.Fail()
+	}
 }
