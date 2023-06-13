8d0a6b333a04394ccea84305bdc68e4a412423fb
Sabariram Chinnathambi <c.sabariram@gmail.com>
2023-06-13 17:59:26 +0530

    Util test case

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,21 +1,13 @@
 package utils_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/sabariramc/goserverbase/v3/utils"
 	"gotest.tools/assert"
 )
 
-func TestGenerateId(t *testing.T) {
-	for i := 0; i < 5000; i++ {
-		x := utils.GenerateId(50, "abc_")
-		assert.Equal(t, len(x), 50)
-		fmt.Println(x)
-	}
-}
-
 func TestGetHash(t *testing.T) {
-	fmt.Println(utils.GetHash("3edcRFV5tgb"))
+	val := "3edcRFV5tgb"
+	assert.Equal(t, utils.GetHash(val), utils.GetHash(val))
 }
