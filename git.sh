a702b93f0fd234aeb5c48846f5716ed7a7a344d7
Sabariram Chinnathambi <sabariram.c@goniyo.com>
2023-11-07 11:44:21 +0530

    Error handling update

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 export message="Error handling update"
-export version="v3.17.1"
-export branch="master"
+export version="v3.18.0.segmentio"
+export branch="segmentio"
 git add .
 git commit -m "$message"
 git tag $version
