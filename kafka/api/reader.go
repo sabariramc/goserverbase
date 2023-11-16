f1a4dde7a3ec00f34b93eeb9b76c6ec4488c46ba
Sabariram Chinnathambi <sabariram.c@goniyo.com>
2023-11-16 10:35:42 +0530

    kafka refactoring

--- kafka/api/reader.go
+++ kafka/api/reader.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"context"
+	"fmt"
+	"sync"
 
 	"github.com/sabariramc/goserverbase/v4/log"
 	"github.com/segmentio/kafka-go"
@@ -9,12 +11,56 @@ import (
 
 type Reader struct {
 	*kafka.Reader
-	log log.Logger
+	log              log.Logger
+	commitLock       sync.Mutex
+	consumedMessages []kafka.Message
+	msgCh            chan *kafka.Message
+	bufferSize       uint64
 }
 
-func NewReader(ctx context.Context, r *kafka.Reader, log log.Logger) *Reader {
+func NewReader(ctx context.Context, log log.Logger, r *kafka.Reader, bufferSize uint64) *Reader {
 	return &Reader{
-		Reader: r,
-		log:    log,
+		Reader:           r,
+		log:              *log.NewResourceLogger("KafkaReader"),
+		consumedMessages: make([]kafka.Message, 0, bufferSize),
+		msgCh:            make(chan *kafka.Message, bufferSize),
+		bufferSize:       bufferSize,
 	}
+}
+
+func (k *Reader) Commit(ctx context.Context) error {
+	k.commitLock.Lock()
+	defer k.commitLock.Unlock()
+	if len(k.consumedMessages) == 0 {
+		return nil
+	}
+	k.log.Debug(ctx, "committing messages", k.consumedMessages)
+	err := k.CommitMessages(ctx, k.consumedMessages...)
+	if err != nil {
+		return fmt.Errorf("kafka.Consumer.Commit: error during commit : %w", err)
+	}
+	k.consumedMessages = make([]kafka.Message, 0, k.bufferSize)
+	return nil
+}
+
+func (k *Reader) Poll(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			m, err := k.FetchMessage(ctx)
+			if err != nil {
+				k.log.Error(ctx, "Poll error", err)
+				return fmt.Errorf("kafka.Consumer.poll: Error: %w", err)
+			}
+			k.msgCh <- &m
+		}
+	}
+}
+
+func (k *Reader) StoreMessage(ctx context.Context, msg *kafka.Message) {
+	k.commitLock.Lock()
+	k.consumedMessages = append(k.consumedMessages, *msg)
+	k.commitLock.Unlock()
 }
