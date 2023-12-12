9afe0dfab4f6cb0d93158be59c5d297a139f20dc
Sabariram Chinnathambi <sabariram.c@goniyo.com>
2023-12-12 18:42:19 +0530

    Test server changes

--- app/server/httpserver/test/server.go
+++ app/server/httpserver/test/server.go
@@ -4,12 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/sabariramc/goserverbase/v4/app/server/httpserver"
+	"github.com/sabariramc/goserverbase/v4/aws"
+	"github.com/sabariramc/goserverbase/v4/db/mongo"
 	"github.com/sabariramc/goserverbase/v4/errors"
+	"github.com/sabariramc/goserverbase/v4/kafka"
 	"github.com/sabariramc/goserverbase/v4/log"
 	"github.com/sabariramc/goserverbase/v4/log/logwriter"
+	"github.com/sabariramc/goserverbase/v4/utils"
+	"github.com/sabariramc/goserverbase/v4/utils/httputil"
 	"github.com/sabariramc/goserverbase/v4/utils/testutils"
 )
 
@@ -18,6 +25,8 @@ var ServerTestLogger *log.Logger
 var ServerTestLMux log.LogMux
 
 func init() {
+	fmt.Println(os.Getwd())
+	testutils.LoadEnv("./test/.env")
 	testutils.Initialize()
 	ServerTestConfig = testutils.NewConfig()
 	consoleLogWriter := logwriter.NewConsoleWriter(log.HostParams{
@@ -36,7 +45,13 @@ func GetCorrelationContext() context.Context {
 
 type server struct {
 	*httpserver.HttpServer
-	log *log.Logger
+	log        *log.Logger
+	pr1        *kafka.Producer
+	pr2        *kafka.Producer
+	conn       *mongo.Mongo
+	coll       *mongo.Collection
+	sns        *aws.SNS
+	httpClient *httputil.HttpClient
 }
 
 func (s *server) Func1(w http.ResponseWriter, r *http.Request) {
@@ -55,6 +70,48 @@ func (s *server) Func2(c *gin.Context) {
 	w.Write([]byte("World"))
 }
 
+func (s *server) testAll(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	data := make(map[string]any)
+	err := s.GetJSONBody(r, &data)
+	if err != nil {
+		s.SetErrorInContext(ctx, errors.NewHTTPClientError(400, "invalidJsonBody", "error marshalling json body", nil, nil, err))
+	}
+	s.coll.InsertOne(ctx, data)
+	msg := utils.NewMessage("testFlight", "test")
+	msg.AddPayload("content", data)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		s.sns.Publish(ctx, &ServerTestConfig.AWS.SNS_ARN, nil, msg, nil)
+	}()
+	go func() {
+		defer wg.Done()
+		res := make(map[string]any)
+		s.httpClient.Post(ctx, "http://localhost:64000/service/a/b", data, &res, nil)
+		s.log.Info(ctx, "http response", res)
+	}()
+	s.pr1.ProduceMessage(ctx, "fasdfa", msg, nil)
+	s.pr1.Flush(ctx)
+	wg.Wait()
+	w.WriteHeader(204)
+}
+
+func (s *server) testKafka(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	data := make(map[string]any)
+	err := s.GetJSONBody(r, &data)
+	if err != nil {
+		s.SetErrorInContext(ctx, errors.NewHTTPClientError(400, "invalidJsonBody", "error marshalling json body", nil, nil, err))
+	}
+	msg := utils.NewMessage("testFlight", "test")
+	msg.AddPayload("content", data)
+	s.pr2.ProduceMessage(ctx, "fasdfa", msg, nil)
+	s.pr2.Flush(ctx)
+	w.WriteHeader(204)
+}
+
 func (s *server) Func3(w http.ResponseWriter, r *http.Request) {
 	s.SetErrorInContext(r.Context(), errors.NewCustomError("hello.new.custom.error", "display this", map[string]any{"one": "two"}, nil, true, nil))
 }
@@ -68,14 +125,36 @@ func (s *server) Func5(w http.ResponseWriter, r *http.Request) {
 }
 
 func NewServer() *server {
+	ctx := GetCorrelationContext()
+	pr1, err := kafka.NewProducer(ctx, ServerTestLogger, ServerTestConfig.KafkaProducer, ServerTestConfig.KafkaTestTopic)
+	if err != nil {
+		ServerTestLogger.Emergency(ctx, "error creating producer1", err, nil)
+	}
+	pr2, err := kafka.NewProducer(ctx, ServerTestLogger, ServerTestConfig.KafkaProducer, ServerTestConfig.KafkaTestTopic2)
+	if err != nil {
+		ServerTestLogger.Emergency(ctx, "error creating producer2", err, nil)
+	}
+	conn, err := mongo.New(ctx, ServerTestLogger, *ServerTestConfig.Mongo)
+	if err != nil {
+		ServerTestLogger.Emergency(ctx, "error creating mongo connection", err, nil)
+	}
 	srv := &server{
 		HttpServer: httpserver.New(*ServerTestConfig.Http, ServerTestLogger, nil), log: ServerTestLogger,
+		pr1:        pr1,
+		pr2:        pr2,
+		sns:        aws.GetDefaultSNSClient(ServerTestLogger),
+		httpClient: httputil.NewDefaultHttpClient(ServerTestLogger),
+		conn:       conn,
+		coll:       conn.Database("GOBaseTest").Collection("TestColl"),
 	}
 	r := srv.GetRouter().Group("/service/v1")
 	tenant := r.Group("/tenant")
 	tenant.GET("", gin.WrapF(srv.Func1))
 	tenant.POST("", gin.WrapF(srv.Func1))
 	tenant.GET("/:tenantId", srv.Func2)
+	resource := r.Group("/test")
+	resource.POST("/all", gin.WrapF(srv.testAll))
+	resource.POST("/kafka2", gin.WrapF(srv.testKafka))
 	errorRoute := r.Group("/error")
 	errorRoute.GET("/error1", gin.WrapF(srv.Func3))
 	errorRoute.GET("/error2", gin.WrapF(srv.Func4))
---
+++ app/server/kafkaconsumer/test/server.go
@@ -0,0 +1,106 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/sabariramc/goserverbase/v4/app/server/kafkaconsumer"
+	"github.com/sabariramc/goserverbase/v4/aws"
+	"github.com/sabariramc/goserverbase/v4/db/mongo"
+	"github.com/sabariramc/goserverbase/v4/errors"
+	"github.com/sabariramc/goserverbase/v4/kafka"
+	"github.com/sabariramc/goserverbase/v4/log"
+	"github.com/sabariramc/goserverbase/v4/log/logwriter"
+	"github.com/sabariramc/goserverbase/v4/utils"
+	"github.com/sabariramc/goserverbase/v4/utils/httputil"
+	"github.com/sabariramc/goserverbase/v4/utils/testutils"
+)
+
+var ServerTestConfig *testutils.TestConfig
+var ServerTestLogger *log.Logger
+var ServerTestLMux log.LogMux
+
+func init() {
+	testutils.LoadEnv("./app/server/kafkaconsumer/test/.env")
+	testutils.Initialize()
+	ServerTestConfig = testutils.NewConfig()
+	consoleLogWriter := logwriter.NewConsoleWriter(log.HostParams{
+		Version:     ServerTestConfig.Logger.Version,
+		Host:        ServerTestConfig.Http.Host,
+		ServiceName: ServerTestConfig.App.ServiceName,
+	})
+	ServerTestLMux = log.NewDefaultLogMux(consoleLogWriter)
+	ServerTestLogger = log.NewLogger(context.TODO(), ServerTestConfig.Logger, "BaseTest", ServerTestLMux, nil)
+}
+
+func GetCorrelationContext() context.Context {
+	ctx := context.WithValue(context.Background(), log.ContextKeyCorrelation, log.GetDefaultCorrelationParam(ServerTestConfig.App.ServiceName))
+	return ctx
+}
+
+type server struct {
+	*kafkaconsumer.KafkaConsumerServer
+	log        *log.Logger
+	pr         *kafka.Producer
+	conn       *mongo.Mongo
+	coll       *mongo.Collection
+	sns        *aws.SNS
+	httpClient *httputil.HttpClient
+}
+
+func (s *server) Func1(ctx context.Context, event *kafka.Message) error {
+	data := make(map[string]any)
+	err := event.LoadBody(&data)
+	if err != nil {
+		return fmt.Errorf("server.Func1: error loading body: %w", err)
+	}
+	s.coll.InsertOne(ctx, data)
+	msg := utils.NewMessage("testFlight", "test")
+	msg.AddPayload("content", data)
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		s.sns.Publish(ctx, &ServerTestConfig.AWS.SNS_ARN, nil, msg, nil)
+	}()
+	go func() {
+		defer wg.Done()
+		res := make(map[string]any)
+		s.httpClient.Post(ctx, "http://localhost:64000/service/a/b", data, &res, nil)
+		s.log.Info(ctx, "http response", res)
+	}()
+	s.pr.ProduceMessage(ctx, "fasdfa", msg, nil)
+	s.pr.Flush(ctx)
+	wg.Wait()
+	return nil
+}
+
+func (s *server) Func2(ctx context.Context, msg *kafka.Message) error {
+	return errors.NewCustomError("hello.new.custom.error", "display this", map[string]any{"one": "two"}, nil, true, nil)
+}
+
+func NewServer() *server {
+	ctx := GetCorrelationContext()
+	pr, err := kafka.NewProducer(ctx, ServerTestLogger, ServerTestConfig.KafkaProducer, ServerTestConfig.KafkaTestTopic2)
+	if err != nil {
+		ServerTestLogger.Emergency(ctx, "error creating producer2", err, nil)
+	}
+	conn, err := mongo.New(ctx, ServerTestLogger, *ServerTestConfig.Mongo)
+	if err != nil {
+		ServerTestLogger.Emergency(ctx, "error creating mongo connection", err, nil)
+	}
+	srv := &server{
+		KafkaConsumerServer: kafkaconsumer.New(*ServerTestConfig.Kafka, ServerTestLogger, nil),
+		pr:                  pr,
+		sns:                 aws.GetDefaultSNSClient(ServerTestLogger),
+		httpClient:          httputil.NewDefaultHttpClient(ServerTestLogger),
+		conn:                conn,
+		coll:                conn.Database("GOBaseTest").Collection("TestColl"),
+	}
+	srv.log = srv.GetLogger().NewResourceLogger("KafkaTestServer")
+	srv.log.Trace(ctx, "config", ServerTestConfig)
+	srv.AddHandler(GetCorrelationContext(), ServerTestConfig.KafkaTestTopic, srv.Func1)
+	srv.AddHandler(GetCorrelationContext(), ServerTestConfig.KafkaTestTopic2, srv.Func2)
+	return srv
+}
