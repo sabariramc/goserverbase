badb2ebe5ba9203df17d442ba5550d409abcc0be
Sabariram <c.sabariram@gmail.com>
2024-02-08 13:51:18 +0530

    updated dependency version

--- git.sh
+++ git.sh
@@ -1,8 +1,9 @@
-export message="kafka buffer overflow"
-export version="v3.18.8.segmentio"
-export branch="segmentio"
+export message="updated dependency version"
+export version="v3.19.0"
+export branch="v3"
 git add .
 git commit -m "$message"
 git tag $version
 git push origin $version
 git push origin $branch
+git push bitbucket $branch
