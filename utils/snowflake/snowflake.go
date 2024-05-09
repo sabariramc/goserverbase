bc4a82942237585bbcbc737ddac9b63ae6f55dfd
Sabariram <c.sabariram@gmail.com>
2024-05-09 16:37:57 +0530

    Added snowflake implementation

---
+++ utils/snowflake/snowflake.go
@@ -0,0 +1,53 @@
+// Package snowflake is a implement of https://en.wikipedia.org/wiki/Snowflake_ID
+package snowflake
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Snowflake struct {
+	machineID      int64
+	sequenceNumber int64
+	lock           sync.Mutex
+}
+
+const maxSequence int64 = 0xFFF
+const maxMachineID int64 = 0x3FF
+const maxTimestamp int64 = 0x1FFFFFFFFFF
+
+func New(machineID, sequenceNumber int64) (*Snowflake, error) {
+	if machineID > maxMachineID {
+		return nil, fmt.Errorf("invalid machine id for Snowflake")
+	}
+	if sequenceNumber > maxSequence {
+		sequenceNumber = 0
+	}
+	return &Snowflake{
+		machineID:      machineID,
+		sequenceNumber: sequenceNumber,
+	}, nil
+}
+
+func (s *Snowflake) Stats() (int64, int64) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.machineID, s.sequenceNumber
+}
+
+func (s *Snowflake) ID() (int64, error) {
+	s.lock.Lock()
+	if s.sequenceNumber == maxSequence {
+		s.sequenceNumber = 0
+	}
+	sequenceNumber := s.sequenceNumber
+	s.sequenceNumber++
+	ts := time.Now().UnixMilli()
+	s.lock.Unlock()
+	if ts > maxTimestamp {
+		return -1, fmt.Errorf("timestamp exceed max limit")
+	}
+	res := ts<<22 | s.machineID<<12 | sequenceNumber
+	return res, nil
+}
---
+++ utils/snowflake/snowflake_test.go
@@ -0,0 +1,81 @@
+package snowflake_test
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/sabariramc/goserverbase/v5/utils/snowflake"
+	"gotest.tools/assert"
+)
+
+func TestSnowflake(t *testing.T) {
+	s, err := snowflake.New(1, 0)
+	assert.NilError(t, err)
+	fmt.Printf("%b\n", time.Now().UnixMilli())
+	for i := 0; i < 10000; i++ {
+		id, err := s.ID()
+		assert.NilError(t, err)
+		fmt.Printf("%b\n", id)
+	}
+}
+
+func TestSnowflakeDuplicacy(t *testing.T) {
+	s, err := snowflake.New(1, 0)
+	assert.NilError(t, err)
+	totalN := 1000000
+	ch := make(chan int64, totalN)
+	var wg sync.WaitGroup
+	conncurrencyFactor := 10000
+	for i := 0; i < conncurrencyFactor; i++ {
+		wg.Add(1)
+		go func() {
+			for j := 0; j < totalN/conncurrencyFactor; j++ {
+				id, err := s.ID()
+				assert.NilError(t, err)
+				ch <- id
+			}
+			wg.Done()
+		}()
+	}
+	wg.Add(1)
+	duplicateCount := 0
+	go func() {
+		idSet := make(map[int64]bool, totalN)
+		total := 0
+		for id := range ch {
+			if _, ok := idSet[id]; ok {
+				duplicateCount++
+			}
+			idSet[id] = true
+			total++
+			if total == totalN {
+				break
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+	assert.Equal(t, duplicateCount, 0)
+}
+
+func BenchmarkSnowflake(b *testing.B) {
+	var v int64
+	s, err := snowflake.New(1, 0)
+	assert.NilError(b, err)
+	var goprocs = runtime.GOMAXPROCS(0)
+	for i := 1; i < 1000; i += 50 {
+		b.Run(fmt.Sprintf("goroutines-%d", i*goprocs), func(b *testing.B) {
+			b.SetParallelism(i)
+			b.RunParallel(func(pb *testing.PB) {
+				for pb.Next() {
+					_, err := s.ID()
+					assert.NilError(b, err)
+					v += 1
+				}
+			})
+		})
+	}
+}
