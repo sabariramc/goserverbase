029595e2e94c04a3ff06af0d828544f9787f1495
Sabariram <c.sabariram@gmail.com>
2024-05-29 20:54:37 +0530

    Rafactored package

--- git.sh
+++ git.sh
@@ -2,7 +2,7 @@ export message="Rafactored package"
 export version="v6.0.0"
 export branch="master"
 git add .
-git commit -m $message"
+git commit -m "$message"
 git tag $version
 git push origin $version
 git push origin $branch
